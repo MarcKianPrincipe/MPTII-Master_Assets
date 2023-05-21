2cc8831629754939ac8c140ff0115e893ab65b81
Marc Kian M. Principe <principe.marckian@gmail.com>
2023-05-21 21:37:48 +0800

    Add project files.

---
+++ mptii-master_assets/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Panel1 = New Panel()
+        txtUser = New TextBox()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Panel2 = New Panel()
+        txtPassword = New TextBox()
+        PictureBox3 = New PictureBox()
+        btnLogin = New Button()
+        btnCancel = New Button()
+        Panel1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.White
+        Panel1.Controls.Add(txtUser)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Location = New Point(30, 124)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(336, 54)
+        Panel1.TabIndex = 0
+        ' 
+        ' txtUser
+        ' 
+        txtUser.BorderStyle = BorderStyle.None
+        txtUser.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        txtUser.Location = New Point(69, 12)
+        txtUser.Name = "txtUser"
+        txtUser.Size = New Size(255, 32)
+        txtUser.TabIndex = 1
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(3, 9)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(60, 35)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(12, 32)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(372, 69)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.White
+        Panel2.Controls.Add(txtPassword)
+        Panel2.Controls.Add(PictureBox3)
+        Panel2.Location = New Point(30, 184)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(336, 54)
+        Panel2.TabIndex = 2
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.BorderStyle = BorderStyle.None
+        txtPassword.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        txtPassword.Location = New Point(69, 12)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(255, 32)
+        txtPassword.TabIndex = 1
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(3, 9)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(60, 35)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 0
+        PictureBox3.TabStop = False
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.BackColor = SystemColors.MenuHighlight
+        btnLogin.Cursor = Cursors.Hand
+        btnLogin.FlatStyle = FlatStyle.Flat
+        btnLogin.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnLogin.ForeColor = Color.White
+        btnLogin.Location = New Point(30, 269)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(337, 39)
+        btnLogin.TabIndex = 3
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = False
+        ' 
+        ' btnCancel
+        ' 
+        btnCancel.BackColor = Color.White
+        btnCancel.Cursor = Cursors.Hand
+        btnCancel.FlatStyle = FlatStyle.Flat
+        btnCancel.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnCancel.ForeColor = Color.Black
+        btnCancel.Location = New Point(30, 314)
+        btnCancel.Name = "btnCancel"
+        btnCancel.Size = New Size(337, 39)
+        btnCancel.TabIndex = 4
+        btnCancel.Text = "Cancel"
+        btnCancel.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonFace
+        ClientSize = New Size(396, 389)
+        Controls.Add(btnCancel)
+        Controls.Add(btnLogin)
+        Controls.Add(Panel2)
+        Controls.Add(PictureBox2)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCancel As Button
+End Class
